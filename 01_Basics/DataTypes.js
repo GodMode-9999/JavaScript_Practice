576b1d6e6bf3280bd3877d12f33f7777e7fe7b99
Vikas Rathod <115883749+GodMode-9999@users.noreply.github.com>
2023-10-14 06:20:05 +0000

    data types and typeof

---
+++ 01_Basics/DataTypes.js
@@ -0,0 +1,22 @@
+/* In older version of javaScript their is no arrow fn , modules & classes are not 
+available */
+"use strict"; //  treat all js code as newer version
+
+// alert (3 + 3) // we are using node.js , in not browser 
+// coding is all about readability
+
+// number => 2 to power 53 
+// bigint 
+// string ="" 
+// boolean = true/false 
+// null => standalone value 
+// undefined =>
+// symbol => unique
+
+// object 
+
+let age = 23
+console.log(typeof('hitesh')) 
+console.log(typeof age) 
+console.log(typeof undefined);
+console.log(typeof 23); 
